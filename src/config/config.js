86a353ca9b0a613e45c38d8881c10de49df84725
Vikas Ukani <vikasuknani5@gmail.com>
2020-02-08 23:19:22 +0530

    Create login page for shiporder login.

--- src/config/config.js
+++ src/config/config.js
@@ -27,56 +27,56 @@ export const api_prefix = baseURL + route_prefix;
  * For Listing Dynamically pagination list drop-down
  */
 export const LIMITS = [
-  {key: "10", value: 10},
-  {key: "25", value: 25},
-  {key: "50", value: 50},
-  {key: "100", value: 100},
-  {key: "150", value: 150},
-  {key: "300", value: 300},
-  {key: "500", value: 500}
+  { key: "10", value: 10 },
+  { key: "25", value: 25 },
+  { key: "50", value: 50 },
+  { key: "100", value: 100 },
+  { key: "150", value: 150 },
+  { key: "300", value: 300 },
+  { key: "500", value: 500 }
 ];
 
 /**
  * All Module APi Collections
  */
 export const ApiCollections = {
-  getImage: {method: "get", url: baseURL},
+  getImage: { method: "get", url: baseURL },
 
-  login: {method: "POST", url: api_prefix + "login"},
-  forgotPassword: {method: "POST", url: api_prefix + "forgot-password"},
-  changePassword: {method: "POST", url: api_prefix + "change-possword"},
+  login: { method: "POST", url: api_prefix + "login" },
+  forgotPassword: { method: "POST", url: api_prefix + "forgot-password" },
+  changePassword: { method: "POST", url: api_prefix + "change-possword" },
 
-  shopper_listing: {method: "GET", url: api_prefix + "shopper-list"},
-  shopper_status_change: {method: "delete", url: api_prefix + "shopper-status-change"},
+  shopper_listing: { method: "GET", url: api_prefix + "shopper-list" },
+  shopper_status_change: { method: "delete", url: api_prefix + "shopper-status-change" },
 
-  users_listing: {method: "GET", url: api_prefix + "users-list"},
-  getUserById: {method: "GET", url: api_prefix + "user/"},
+  users_listing: { method: "GET", url: api_prefix + "users-list" },
+  getUserById: { method: "GET", url: api_prefix + "user/" },
   update_user: {
     method: "post",
     url: api_prefix + "user-update",
-    headers: {headers: {"content-type": "multipart/form-data"}}
+    headers: { headers: { "content-type": "multipart/form-data" } }
   },
-  update_profile: {method: "post", url: api_prefix + "user-profile"},
+  update_profile: { method: "post", url: api_prefix + "user-profile" },
   upload_image: {
     method: "post",
     url: api_prefix + "upload",
-    headers: {headers: {"Content-Type": "multipart/form-data"}}
+    headers: { headers: { "Content-Type": "multipart/form-data" } }
   },
 
   singleImageUpload: {
     method: "post",
     url: baseURL + "/single-image-upload",
-    headers: {headers: {"Content-Type": "multipart/form-data"}}
+    headers: { headers: { "Content-Type": "multipart/form-data" } }
   },
-  users_status_change: {method: "delete", url: api_prefix + "users-status-change"},
-  get_users_count_by_type: {method: "get", url: api_prefix + "get-user-count-by-type"},
+  users_status_change: { method: "delete", url: api_prefix + "users-status-change" },
+  get_users_count_by_type: { method: "get", url: api_prefix + "get-user-count-by-type" },
   /** Masters Countries countries */
-  countries_listing: {method: "POST", url: api_prefix + "countries-list"},
-  create_countries: {method: "POST", url: api_prefix + "countries-create"},
-  update_countries: {method: "put", url: api_prefix + "countries-update/"},
-  get_countries: {method: "GET", url: api_prefix + "countries-show/"},
-  delete_countries: {method: "delete", url: api_prefix + "countries-delete/"},
-  countries_status_change: {method: "delete", url: api_prefix + "countries-status-change"},
+  countries_listing: { method: "POST", url: api_prefix + "countries-list" },
+  create_countries: { method: "POST", url: api_prefix + "countries-create" },
+  update_countries: { method: "put", url: api_prefix + "countries-update/" },
+  get_countries: { method: "GET", url: api_prefix + "countries-show/" },
+  delete_countries: { method: "delete", url: api_prefix + "countries-delete/" },
+  countries_status_change: { method: "delete", url: api_prefix + "countries-status-change" },
   countries_delete_multiple: {
     method: "POST",
     url: api_prefix + "countries-status-delete-many"
@@ -84,11 +84,11 @@ export const ApiCollections = {
 
 
   /** Masters Complaint categories */
-  complaint_category_listing: {method: "POST", url: api_prefix + "complaint-category-list"},
-  create_complaint_category: {method: "POST", url: api_prefix + "complaint-category-create"},
-  update_complaint_category: {method: "put", url: api_prefix + "complaint-category-update/"},
-  get_complaint_category: {method: "GET", url: api_prefix + "complaint-category-show/"},
-  delete_complaint_category: {method: "delete", url: api_prefix + "complaint-category-delete/"},
+  complaint_category_listing: { method: "POST", url: api_prefix + "complaint-category-list" },
+  create_complaint_category: { method: "POST", url: api_prefix + "complaint-category-create" },
+  update_complaint_category: { method: "put", url: api_prefix + "complaint-category-update/" },
+  get_complaint_category: { method: "GET", url: api_prefix + "complaint-category-show/" },
+  delete_complaint_category: { method: "delete", url: api_prefix + "complaint-category-delete/" },
   complaint_category_status_change: {
     method: "delete",
     url: api_prefix + "complaint-category-status-change"
@@ -99,52 +99,52 @@ export const ApiCollections = {
   },
 
   /** Categories Apis */
-  category_list: {method: "POST", url: api_prefix + "category-list"},
-  category_create: {method: "POST", url: api_prefix + "category-create"},
-  category_get: {method: "GET", url: api_prefix + "category-show/"},
-  category_update: {method: "POST", url: api_prefix + "category-update/"},
-  category_delete_multiple: {method: "delete", url: api_prefix + "category-delete-multiple"},
-  category_delete: {method: "get", url: api_prefix + "category-delete/"},
-  category_update_status_change: {method: "POST", url: api_prefix + "category-status-change"},
+  category_list: { method: "POST", url: api_prefix + "category-list" },
+  category_create: { method: "POST", url: api_prefix + "category-create" },
+  category_get: { method: "GET", url: api_prefix + "category-show/" },
+  category_update: { method: "POST", url: api_prefix + "category-update/" },
+  category_delete_multiple: { method: "delete", url: api_prefix + "category-delete-multiple" },
+  category_delete: { method: "get", url: api_prefix + "category-delete/" },
+  category_update_status_change: { method: "POST", url: api_prefix + "category-status-change" },
 
   /** categories api */
-  categories_list: {method: "POST", url: api_prefix + "categories-list"},
+  categories_list: { method: "POST", url: api_prefix + "categories-list" },
 
   /**
    * Feature Products List
    */
-  feature_product_list: {method: "POST", url: api_prefix + "feature-product-list"},
-  feature_product_create: {method: "POST", url: api_prefix + "feature-product-create"},
-  feature_product_get: {method: "GET", url: api_prefix + "feature-product-show/"},
-  feature_product_update: {method: "POST", url: api_prefix + "feature-product-update/"},
-  feature_product_delete_multiple: {method: "delete", url: api_prefix + "feature-product-delete-multiple"},
-  feature_product_delete: {method: "get", url: api_prefix + "feature-product-delete/"},
-  feature_product_update_status_change: {method: "POST", url: api_prefix + "feature-product-status-change"},
+  feature_product_list: { method: "POST", url: api_prefix + "feature-product-list" },
+  feature_product_create: { method: "POST", url: api_prefix + "feature-product-create" },
+  feature_product_get: { method: "GET", url: api_prefix + "feature-product-show/" },
+  feature_product_update: { method: "POST", url: api_prefix + "feature-product-update/" },
+  feature_product_delete_multiple: { method: "delete", url: api_prefix + "feature-product-delete-multiple" },
+  feature_product_delete: { method: "get", url: api_prefix + "feature-product-delete/" },
+  feature_product_update_status_change: { method: "POST", url: api_prefix + "feature-product-status-change" },
 
   /**
    * Product Common Attribute APIs
    */
-  common_products_attributes_list: {method: "POST", url: api_prefix + "common-products-attributes-list"},
+  common_products_attributes_list: { method: "POST", url: api_prefix + "common-products-attributes-list" },
 
 
   /** products apis */
-  products_list: {method: "POST", url: api_prefix + "products-list"},
-  products_create: {method: "POST", url: api_prefix + "products-create"},
-  products_get: {method: "GET", url: api_prefix + "products-show/"},
-  products_update: {method: "POST", url: api_prefix + "products-update/"},
-  products_delete_multiple: {method: "delete", url: api_prefix + "products-delete-multiple"},
-  products_delete: {method: "get", url: api_prefix + "products-delete/"},
-  products_update_status_change: {method: "POST", url: api_prefix + "products-status-change"},
+  products_list: { method: "POST", url: api_prefix + "products-list" },
+  products_create: { method: "POST", url: api_prefix + "products-create" },
+  products_get: { method: "GET", url: api_prefix + "products-show/" },
+  products_update: { method: "POST", url: api_prefix + "products-update/" },
+  products_delete_multiple: { method: "delete", url: api_prefix + "products-delete-multiple" },
+  products_delete: { method: "get", url: api_prefix + "products-delete/" },
+  products_update_status_change: { method: "POST", url: api_prefix + "products-status-change" },
 
   /**
    * Offer Modules
    */
 
   /** offers apis */
-  offers_list: {method: "POST", url: api_prefix + "offers-list"},
-  offers_create: {method: "POST", url: api_prefix + "offers-create"},
-  offers_get: {method: "GET", url: api_prefix + "offers-show/"},
-  offers_update: {method: "POST", url: api_prefix + "offers-update/"},
+  offers_list: { method: "POST", url: api_prefix + "offers-list" },
+  offers_create: { method: "POST", url: api_prefix + "offers-create" },
+  offers_get: { method: "GET", url: api_prefix + "offers-show/" },
+  offers_update: { method: "POST", url: api_prefix + "offers-update/" },
   offers_delete_multiple: {
     method: "delete",
     url: api_prefix + "offers-delete-multiple"
@@ -159,4 +159,19 @@ export const ApiCollections = {
   }
 
 
+
+
+
 };
+
+
+
+export const ShipOrderAPICollection = {
+  // shiporder-login
+  login: { method: "POST", url: api_prefix + "shiporder-login" },
+
+  // login: {
+  //   method: "POST", url: "https://apiv2.shiprocket.in/v1/external/auth/login", headers: { headers: { "Content-Type": "application/json" } }
+  // },
+
+}
--- src/views/admin/ShiporderLogin/Login.vue
+++ src/views/admin/ShiporderLogin/Login.vue
@@ -1,10 +1,67 @@
 <template>
   <div>
-    <h2>Ship Order Login</h2>
+    <div class="app flex-row">
+      <!-- align-items-center -->
+      <div class="container">
+        <b-row class>
+          <!-- justify-content-center -->
+          <b-col md="8">
+            <b-card-group>
+              <b-card no-body class="p-4">
+                <b-card-body>
+                  <b-form>
+                    <h1>Login</h1>
+                    <p class="text-muted">Login at Shiporder</p>
+                    <b-input-group class="mb-3">
+                      <b-input-group-prepend>
+                        <b-input-group-text>
+                          <i class="icon-user"></i>
+                        </b-input-group-text>
+                      </b-input-group-prepend>
+                      <b-form-input
+                        type="text"
+                        class="form-control"
+                        v-model="login.email"
+                        placeholder="Username"
+                        autocomplete="username email"
+                      />
+                    </b-input-group>
+                    <b-input-group class="mb-4">
+                      <b-input-group-prepend>
+                        <b-input-group-text>
+                          <i class="icon-lock"></i>
+                        </b-input-group-text>
+                      </b-input-group-prepend>
+                      <b-form-input
+                        type="password"
+                        class="form-control"
+                        v-model="login.password"
+                        placeholder="Password"
+                        autocomplete="current-password"
+                      />
+                    </b-input-group>
+                    <b-row>
+                      <b-col cols="6">
+                        <b-button variant="primary" class="px-4" @click="loginCall">Login</b-button>
+                      </b-col>
+                      <!-- <b-col cols="6" class="text-right">
+                        <b-button variant="link" class="px-0">Forgot password?</b-button>
+                      </b-col>-->
+                    </b-row>
+                  </b-form>
+                </b-card-body>
+              </b-card>
+            </b-card-group>
+          </b-col>
+        </b-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import apiServices from "../../../Services/apiServices";
+import { ShipOrderAPICollection } from "../../../config/config";
 export default {
   name: "ShiporderLogin",
   data() {
@@ -13,7 +70,29 @@ export default {
     };
   },
   methods: {
-    loginCall() {}
+    async loginCall() {
+      console.log("clicked", this.login);
+
+      // fetch(ShipOrderAPICollection.login.url, {
+      //   // Adding method type
+      //   method: "POST",
+      //   // Adding body or contents to send
+      //   body: JSON.stringify(this.login),
+      //   // Adding headers to the request
+      //   ...ShipOrderAPICollection.login.headers
+      // })
+      //   // Converting to JSON
+      //   .then(response => response.json())
+      //   .then(json => console.log("json", json));
+
+      let res = await apiServices
+        .call(ShipOrderAPICollection.login)
+        .post(this.login);
+
+      console.log("res", res);
+
+      // if ( res && )
+    }
   }
 };
 </script>
